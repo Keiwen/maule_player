7414dd11b0eebf94ff940ff54f33ed5aaae0e159
keiwen <keiwen.mk@gmail.com>
2023-09-01 11:42:27 +0200

    remove unused stuff

--- src/Controller/AbstractAppController.php
+++ src/Controller/AbstractAppController.php
@@ -2,18 +2,10 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
 use Keiwen\Cacofony\Controller\AppController;
 
 class AbstractAppController extends AppController
 {
-    /**
-     * @return User
-     */
-    protected function getUser()
-    {
-        return parent::getUser();
-    }
 
 
 }
