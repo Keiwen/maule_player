9c803ac08a68843a47913af73cc48e558a359e82
keiwen <keiwen.mk@gmail.com>
2023-09-13 15:08:34 +0200

    feat: MLP-6 Get all track from DB
    Prepare main API controller with links for auto-discovery

--- src/Controller/API/APIController.php
+++ src/Controller/API/APIController.php
@@ -5,6 +5,7 @@ namespace App\Controller\API;
 use Keiwen\Cacofony\Controller\AppController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 /**
  * Class APIController
@@ -12,6 +13,29 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class APIController extends AppController
 {
+
+    const ATTRIBUTE_API_LINK = 'links';
+
+    /**
+     * @param array $data data where to add links
+     * @param string $routeName
+     * @param array $parameters route parameters
+     * @param string $rel link relation
+     * @param string $method link HTTP method
+     *
+     * @see https://www.iana.org/assignments/link-relations/link-relations.xhtml
+     */
+    protected function addApiLink(array &$data, string $routeName, array $parameters = array(), string $rel = 'self', string $method = 'GET')
+    {
+        $url = $this->generateUrl($routeName, $parameters, UrlGeneratorInterface::ABSOLUTE_URL);
+        $linkData =  array(
+            'href' => $url,
+            'rel' => $rel,
+            'method' => $method
+        );
+        if (!isset($data[self::ATTRIBUTE_API_LINK])) $data[self::ATTRIBUTE_API_LINK] = array();
+        $data[self::ATTRIBUTE_API_LINK][] = $linkData;
+    }
 
     /**
      * @param array $data
