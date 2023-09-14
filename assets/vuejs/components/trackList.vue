61a8711de04f8dfb50c620228843bea5f028d3dc
keiwen <keiwen.mk@gmail.com>
2023-09-14 16:39:02 +0200

    feat: MLP-7 Homepage - showing all titles
    prepare components track item and list

---
+++ assets/vuejs/components/trackList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+
+    <ul class="list-group">
+      <li class="list-group-item container" v-for="track in trackList">
+        <track-list-item :track="track"></track-list-item>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import trackListItem from "./trackListItem";
+
+export default {
+  name: "trackList",
+  components: { trackListItem },
+  props: {
+    trackList: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+  .list-group-item {
+    background-color: transparent;
+    padding: .2rem 0.5rem;
+    border: 1px solid var(--primary);
+  }
+
+  .track-list-empty {
+    font-style: italic;
+    font-size: 0.8rem;
+  }
+
+</style>
---
+++ assets/vuejs/components/trackListItem.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="row">
+
+    <div class="container-fluid row">
+      <div class="col-12">
+        <span class="track-title">{{ limitTitle(track.name, 30) }}</span>
+      </div>
+    </div>
+    <div class="container-fluid row">
+      <div class="col-12">
+        <span class="track-artist">{{ limitTitle(track.artist.name, 30) }}</span>
+      </div>
+    </div>
+
+    <div class="container-fluid row">
+      <div class="col-2">
+        <span class="track-number">#{{ track.trackNumber }}</span>
+      </div>
+      <div class="col-8">
+        <span class="track-album">{{ limitTitle(track.album.name, 20) }}</span>
+      </div>
+      <div class="col-2">
+        <span class="track-year">{{ track.year }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {mapGetters} from "vuex";
+
+export default {
+  name: "trackListItem",
+  props: {
+    track: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    ...mapGetters(['limitTitle'])
+  }
+}
+</script>
+
+<style scoped>
+
+.track-title {
+  font-size: 1.2rem;
+  font-weight: bold;
+}
+.track-artist {
+  font-weight: bold;
+}
+
+</style>
