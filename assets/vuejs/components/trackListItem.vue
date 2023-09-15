0859f06ae232f5e7bf35653a4424e67f8667b8cd
keiwen <keiwen.mk@gmail.com>
2023-09-15 18:54:47 +0200

    feat: MLP-10 Play music
    add play button to change state

--- assets/vuejs/components/trackListItem.vue
+++ assets/vuejs/components/trackListItem.vue
@@ -6,28 +6,36 @@
         <span class="track-title">{{ limitTitle(track.name, 30) }}</span>
       </div>
     </div>
-    <div class="container-fluid row">
-      <div class="col-12">
-        <span class="track-artist">{{ limitTitle(track.artist.name, 30) }}</span>
-      </div>
-    </div>
 
     <div class="container-fluid row">
+
       <div class="col-2">
-        <span class="track-number">#{{ track.trackNumber }}</span>
+        <button class="btn btn-primary btn-play" @click="selectTrack">
+          <v-icon name="play"/>
+        </button>
       </div>
-      <div class="col-8">
-        <span class="track-album">{{ limitTitle(track.album.name, 20) }}</span>
+      <div class="col-10 row">
+        <div class="col-12">
+          <span class="track-artist">{{ limitTitle(track.artist.name, 30) }}</span>
+        </div>
+
+        <div class="col-2">
+          <span class="track-number">#{{ track.trackNumber }}</span>
+        </div>
+        <div class="col-8">
+          <span class="track-album">{{ limitTitle(track.album.name, 20) }}</span>
+        </div>
+        <div class="col-2">
+          <span class="track-year">{{ track.year }}</span>
+        </div>
       </div>
-      <div class="col-2">
-        <span class="track-year">{{ track.year }}</span>
-      </div>
+
     </div>
   </div>
 </template>
 
 <script>
-import {mapGetters} from "vuex";
+import {mapGetters, mapActions} from "vuex";
 
 export default {
   name: "trackListItem",
@@ -39,11 +47,17 @@ export default {
   },
   computed: {
     ...mapGetters(['limitTitle'])
+  },
+  methods: {
+    ...mapActions(['setPlayerSrc']),
+    selectTrack() {
+      this.setPlayerSrc(this.track.filepath)
+    }
   }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 
 .track-title {
   font-size: 1.2rem;
@@ -51,6 +65,10 @@ export default {
 }
 .track-artist {
   font-weight: bold;
+}
+
+.btn-play {
+  color: var(--background);
 }
 
 </style>
