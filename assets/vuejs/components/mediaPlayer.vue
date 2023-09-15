c4976c48057e2d2ef8ede4a294a074a01f134646
keiwen <keiwen.mk@gmail.com>
2023-09-15 16:54:24 +0200

    feat: MLP-9 VueJS media player
    set simple version of audio player

--- assets/vuejs/components/mediaPlayer.vue
+++ assets/vuejs/components/mediaPlayer.vue
@@ -1,15 +1,65 @@
 <template>
-  <div class="container-fluid">
-    This will be the media player
+  <div class="audio-container">
+    <audio controls>
+      <source ref="audio_player" :src="mediaSrc">
+    </audio>
   </div>
 </template>
 
 <script>
+import {mapGetters} from "vuex";
+
 export default {
-  name: "mediaPlayer"
+  name: "mediaPlayer",
+  computed: {
+    ...mapGetters(['playedMediaFilepath']),
+    mediaSrc () {
+      return '/media_lib/' + this.playedMediaFilepath;
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+
+.audio-container {
+  width: 100%;
+}
+
+/* container */
+audio::-webkit-media-controls-panel {
+  background-color: var(--primary);
+  width: 100%;
+}
+
+/* play button */
+audio::-webkit-media-controls-play-button {
+  background-color: var(--secondary);
+  border-radius: 50%;
+}
+
+/* volume button */
+audio::-webkit-media-controls-mute-button {
+  background-color: var(--secondary);
+  border-radius: 50%;
+}
+
+/* current time */
+audio::-webkit-media-controls-current-time-display {
+  color: var(--white);
+}
+
+/* remaining time */
+audio::-webkit-media-controls-time-remaining-display {
+  color: var(--white);
+}
+
+/* timeline */
+audio::-webkit-media-controls-timeline {
+  background-color: var(--background);
+  border-radius: 25px;
+  margin-left: 10px;
+  margin-right: 10px;
+}
 
 </style>
