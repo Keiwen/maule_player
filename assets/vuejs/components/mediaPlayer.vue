bb00a3a504a95f10160164738c60dc325009a22c
keiwen <keiwen.mk@gmail.com>
2023-09-20 10:51:01 +0200

    feat: MLP-20 Style media player
    timeline component draft

--- assets/vuejs/components/mediaPlayer.vue
+++ assets/vuejs/components/mediaPlayer.vue
@@ -1,13 +1,27 @@
 <template>
   <div class="container audio-container">
     <div class="custom-player-container row">
-      <div class="col-2">
+      <div class="col-2 custom-player-play">
         <div class="row">
           <play-button :playing="playingAudio" @click-play="togglePlay" color="var(--light)" />
         </div>
       </div>
-      <div class="col-8">Custom player middle</div>
-      <div class="col-2">right</div>
+      <div class="col-7 custom-player-middle">
+        <div class="row custom-player-text">Text</div>
+        <div class="row custom-player-timeline">
+          <timeline percent-progress="50" />
+        </div>
+      </div>
+      <div class="col-3 custom-player-prevnext">
+        <div class="row">Right</div>
+        <div class="row">
+          <span class="custom-player-time">
+            <span class="custom-player-current-time">0:00</span>
+            /
+            <span class="custom-player-duration">0:00</span>
+          </span>
+        </div>
+      </div>
     </div>
 
 
@@ -20,10 +34,11 @@
 <script>
 import {mapGetters} from "vuex";
 import playButton from "./playButton";
+import timeline from "./timeline";
 
 export default {
   name: "mediaPlayer",
-  components: { playButton },
+  components: { playButton, timeline },
   data () {
     return {
       audioElement: null,
@@ -67,40 +82,39 @@ export default {
 }
 
 
-/* container */
-audio::-webkit-media-controls-panel {
-  background-color: var(--primary);
-  width: 100%;
+audio {
+  // CHROME DEFAULT AUDIO
+  // container
+  &::-webkit-media-controls-panel {
+    background-color: var(--primary);
+    width: 100%;
+  }
+  // play button
+  &::-webkit-media-controls-play-button {
+    background-color: var(--secondary);
+    border-radius: 50%;
+  }
+  // volume button
+  &::-webkit-media-controls-mute-button {
+    background-color: var(--secondary);
+    border-radius: 50%;
+  }
+  // current time
+  &::-webkit-media-controls-current-time-display {
+    color: var(--white);
+  }
+  // remaining time
+  &::-webkit-media-controls-time-remaining-display {
+    color: var(--white);
+  }
+  // timeline
+  &::-webkit-media-controls-timeline {
+    background-color: var(--background);
+    border-radius: 25px;
+    margin-left: 10px;
+    margin-right: 10px;
+  }
 }
 
-/* play button */
-audio::-webkit-media-controls-play-button {
-  background-color: var(--secondary);
-  border-radius: 50%;
-}
-
-/* volume button */
-audio::-webkit-media-controls-mute-button {
-  background-color: var(--secondary);
-  border-radius: 50%;
-}
-
-/* current time */
-audio::-webkit-media-controls-current-time-display {
-  color: var(--white);
-}
-
-/* remaining time */
-audio::-webkit-media-controls-time-remaining-display {
-  color: var(--white);
-}
-
-/* timeline */
-audio::-webkit-media-controls-timeline {
-  background-color: var(--background);
-  border-radius: 25px;
-  margin-left: 10px;
-  margin-right: 10px;
-}
 
 </style>
---
+++ assets/vuejs/components/timeline.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="custom-player-timeline">
+    <input type="range" max="100" value="0">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "timeline",
+  props: {
+    percentProgress: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+.custom-player-timeline {
+  width: 98%;
+  input[type="range"] {
+    width: 100%;
+    cursor: pointer;
+    -webkit-appearance: none;
+    height: 25px;
+    background-color: var(--background);
+    padding: 5px 10px;
+    border-radius: 25px;
+    position: relative;
+
+
+    //CHROME
+    &::-webkit-slider-runnable-track {
+      height: 3px;
+      background: rgba(0, 0, 0, 0.3);
+    }
+    &::-webkit-slider-thumb {
+      position: relative;
+      -webkit-appearance: none;
+      box-sizing: content-box;
+      height: 18px;
+      width: 18px;
+      border-radius: 50%;
+      background-color: var(--secondary);
+      margin: -8px 0 0 0;
+    }
+    &:active::-webkit-slider-thumb {
+      transform: scale(1.2);
+    }
+    // next element is used to display timeline before thumb
+    // however, we need to compute its width and dynamically adjust it
+    // not shown on firefox, replaced by range-progress pseudo element
+    &::before {
+      position: absolute;
+      content: "";
+      top: 11px;
+      left: 10px;
+      width: 50px;
+      height: 3px;
+      background-color: var(--dark);
+    }
+
+
+    //FIREFOX
+    &::-moz-range-track {
+      height: 3px;
+      background: rgba(0, 0, 0, 0.3);
+    }
+    &::-moz-range-progress {
+      background-color: var(--dark);
+    }
+    &::-moz-range-thumb {
+      position: relative;
+      -webkit-appearance: none;
+      box-sizing: content-box;
+      height: 18px;
+      width: 18px;
+      border-radius: 50%;
+      background-color: var(--secondary);
+      margin: -8px 0 0 0;
+    }
+    &:active::-moz-range-thumb {
+      transform: scale(1.2);
+    }
+
+
+    //EDGE
+    &::-ms-track {
+      width: 100%;
+      height: 3px;
+      background: transparent;
+      border: solid transparent;
+      color: transparent;
+    }
+    &::-ms-fill-lower {
+      background-color: var(--dark);
+    }
+    &::-ms-fill-upper {
+      background: rgba(0, 0, 0, 0.3);
+    }
+    &::-ms-thumb {
+      position: relative;
+      -webkit-appearance: none;
+      box-sizing: content-box;
+      height: 18px;
+      width: 18px;
+      border-radius: 50%;
+      background-color: var(--secondary);
+      margin: -8px 0 0 0;
+    }
+    &:active::-ms-thumb {
+      transform: scale(1.2);
+    }
+  }
+}
+
+</style>
