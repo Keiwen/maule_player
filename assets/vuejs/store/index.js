335c916026eb784e827563770320c989f16ea5b6
keiwen <keiwen.mk@gmail.com>
2023-09-21 16:36:30 +0200

    feat: MLP-20 Style media player
    add store getter to display time

--- assets/vuejs/store/index.js
+++ assets/vuejs/store/index.js
@@ -24,6 +24,31 @@ export default new Vuex.Store({
     getLimitedTitle: () => (title, limit = 20) => {
       if (title.length <= limit) return title
       return title.substring(0, limit - 1) + '...'
+    },
+    getDisplayTime: () => (timeInSecond) => {
+      let partHours = 0
+      let partMinutes = 0
+      let timeToWork = 0
+      let partSeconds = Math.round(timeInSecond)
+      if (partSeconds > 60) {
+        timeToWork = partSeconds / 60
+        partMinutes = Math.floor(timeToWork)
+        partSeconds = Math.round((timeToWork - partMinutes) * 60)
+      }
+      if (partMinutes > 60) {
+        timeToWork = partMinutes / 60
+        partHours = Math.floor(timeToWork)
+        partMinutes = Math.round((timeToWork - partHours) * 60)
+      }
+      if (partSeconds < 10) partSeconds = '0' + partSeconds
+      if (partMinutes < 10) partMinutes = '0' + partMinutes
+
+      let displayTime = partMinutes + ':' + partSeconds
+      if (partHours > 0) {
+        displayTime = partHours + ':' + displayTime
+      }
+      return displayTime
+
     }
   },
   actions: {
