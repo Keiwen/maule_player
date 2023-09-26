3a9d53cc67f9213fdcfa2005633c1557ee54e45b
keiwen <keiwen.mk@gmail.com>
2023-09-26 14:59:29 +0200

    feat: MLP-16 Persist import date to avoid crawling full media lib
    Check last modification timestamp

--- src/Parser/MediaLibDirectoryParser.php
+++ src/Parser/MediaLibDirectoryParser.php
@@ -10,18 +10,20 @@ class MediaLibDirectoryParser extends DirectoryParser
 
     protected $output;
     protected $allowedExtensions;
+    protected $ignoreBeforeTimestamp;
 
     public function __construct(
         string $baseDirectory,
         string $pathSeparator = '/',
         array $allowedExtensions = array(),
-        OutputInterface $output = null
+        OutputInterface $output = null,
+        int $ignoreBeforeTimestamp = -1
     )
     {
         $this->allowedExtensions = $allowedExtensions;
         $this->output = $output;
+        $this->ignoreBeforeTimestamp = $ignoreBeforeTimestamp;
         parent::__construct($baseDirectory, $pathSeparator);
-        // TODO include starting time and check if file/folder is after or not
     }
 
 
@@ -30,19 +32,29 @@ class MediaLibDirectoryParser extends DirectoryParser
         $extension = static::getFileExtension($filename);
         if (!in_array($extension, $this->allowedExtensions)) {
             if ($this->output) {
-                $this->output->writeln(sprintf('   Ignored file %s%s as extension %s not allowed', $directory, $filename, $extension));
+                $this->output->writeln(sprintf('   Ignored file %s%s: extension %s not allowed', $directory, $filename, $extension));
             }
             return false;
         }
 
-        //TODO check modified date with filemtime($this->baseDirectory . $directory . $filename)
+        if ($this->ignoreBeforeTimestamp >= 0 && filemtime($this->baseDirectory . $directory . $filename) < $this->ignoreBeforeTimestamp) {
+            if ($this->output) {
+                $this->output->writeln(sprintf('   Ignored file %s%s: not modified since last execution', $directory, $filename));
+            }
+            return false;
+        }
 
         return true;
     }
 
     protected function doesConsiderFolder(string $directory, string $foldername): bool
     {
-        //TODO check modified date with filemtime($this->baseDirectory . $directory . $foldername)
+        if ($this->ignoreBeforeTimestamp >= 0 && filemtime($this->baseDirectory . $directory . $foldername) < $this->ignoreBeforeTimestamp) {
+            if ($this->output) {
+                $this->output->writeln(sprintf('   Ignored folder %s%s: not modified since last execution', $directory, $foldername));
+            }
+            return false;
+        }
 
         return true;
     }
