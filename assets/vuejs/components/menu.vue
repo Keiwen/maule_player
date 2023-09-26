6a7421632cb563557fb35c32cdf9235cb41c3021
keiwen <keiwen.mk@gmail.com>
2023-09-26 15:44:32 +0200

    feat: reset state temp button

--- assets/vuejs/components/menu.vue
+++ assets/vuejs/components/menu.vue
@@ -7,14 +7,19 @@
       <router-link :to="{name: 'main'}" class="btn btn-primary btn-menu btn-menu-middle">Home</router-link>
     </div>
     <div class="col-4 text-center">
-      <router-link :to="{name: 'main'}" class="btn btn-primary btn-menu">Home</router-link>
+      <button class="btn btn-primary btn-menu" @click="resetState">Reset</button>
     </div>
   </div>
 </template>
 
 <script>
+import {mapActions} from "vuex";
+
 export default {
-  name: "appMenu"
+  name: "appMenu",
+  methods: {
+    ...mapActions(['resetState'])
+  }
 }
 </script>
 
