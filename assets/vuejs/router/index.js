7eb3653b76f5537fabd19f5769f5613928b8ac46
keiwen <keiwen.mk@gmail.com>
2023-09-27 15:55:27 +0200

    feat: MLP-26 All artists page
    new page in router

--- assets/vuejs/router/index.js
+++ assets/vuejs/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Main from '../pages/main'
+import Artists from '../pages/artists'
 
 Vue.use(Router)
 
@@ -15,6 +16,11 @@ export default new Router({
             path: '/',
             name: 'main',
             component: Main
+        },
+        {
+            path: '/artists/',
+            name: 'artists',
+            component: Artists
         }
     ]
 })
