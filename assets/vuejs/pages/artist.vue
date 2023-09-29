a31094e9d5992f25f921e946e6a79a4992d8ecec
keiwen <keiwen.mk@gmail.com>
2023-09-29 12:56:22 +0200

    feat: MLP-33 Display discography on artist page

--- assets/vuejs/pages/artist.vue
+++ assets/vuejs/pages/artist.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1><artist-icon /> {{ artist.name }}</h1>
+    <h2>{{ this.$trans('artist.discography', {}, null, true) }}</h2>
+    <album-list :album-list="albumList" :allowSearch="false"></album-list>
+
     <h2>{{ this.$trans('artist.recent_tracks', {}, null, true) }}</h2>
     <track-list :track-list="trackList" :allowSearch="false"></track-list>
   </div>
@@ -11,48 +14,70 @@ import {useRemoteCall} from "../composables/useRemoteCall";
 import {mapActions} from "vuex";
 import ArtistIcon from "../components/artistIcon";
 import TrackList from "../components/trackList";
+import AlbumList from "../components/albumList";
 
 export default {
   name: "artistPage",
-  components: { TrackList, ArtistIcon },
+  components: { TrackList, ArtistIcon, AlbumList },
   data () {
     return {
       artist: {},
-      remoteCallData: {},
-      remoteCallError: null,
-      trackList: []
+      remoteCallTrackData: {},
+      remoteCallTrackError: null,
+      remoteCallAlbumData: {},
+      remoteCallAlbumError: null,
+      trackList: [],
+      albumList: []
     }
   },
   computed: {
     isLoading () {
-      return (this.remoteCallData == null && this.remoteCallError == null)
+      return (this.remoteCallTrackData == null && this.remoteCallTrackError == null
+        && this.remoteCallAlbumData == null && this.remoteCallAlbumError == null)
     },
   },
   watch: {
-    remoteCallData: function(newValue, oldValue) {
-      if (newValue !== null && this.remoteCallError === null) {
+    remoteCallTrackData: function(newValue, oldValue) {
+      if (newValue !== null && this.remoteCallTrackError === null) {
         this.trackList = newValue.tracks
       }
     },
-    remoteCallError: function(newValue, oldValue) {
-      if (newValue !== null && this.remoteCallData === null) {
+    remoteCallTrackError: function(newValue, oldValue) {
+      if (newValue !== null && this.remoteCallTrackData === null) {
         this.addError(this.$trans('track.list.error', {}, null, true))
+      }
+    },
+    remoteCallAlbumData: function(newValue, oldValue) {
+      if (newValue !== null && this.remoteCallAlbumError === null) {
+        this.albumList = newValue.albums
+      }
+    },
+    remoteCallAlbumError: function(newValue, oldValue) {
+      if (newValue !== null && this.remoteCallAlbumData === null) {
+        this.addError(this.$trans('album.list.error', {}, null, true))
       }
     }
   },
   mounted () {
     if (this.$route.params.artist !== undefined) {
       this.artist = this.$route.params.artist
-      this.updateList()
+      this.updateAlbumList()
+      this.updateTrackList()
     }
   },
   methods: {
     ...mapActions(['addError']),
-    updateList () {
+    updateTrackList () {
       const urlToCall = this.$url(URL_API.artist_tracks, {id: this.artist.id, limit: 5})
       const {callData, callError} = useRemoteCall(urlToCall)
-      this.remoteCallData = callData
-      this.remoteCallError = callError
+      this.remoteCallTrackData = callData
+      this.remoteCallTrackError = callError
+    },
+    updateAlbumList () {
+      const urlToCall = this.$url(URL_API.artist_albums, {id: this.artist.id})
+      const {callData, callError} = useRemoteCall(urlToCall)
+      this.remoteCallAlbumData = callData
+      this.remoteCallAlbumError = callError
     }
   }
 }
