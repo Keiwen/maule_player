348d071251145f3a3f495574d25589853742b8ac
keiwen <keiwen.mk@gmail.com>
2023-09-30 22:24:06 +0200

    feat: MLP-44 Settings page
    add change locale buttons

--- assets/vuejs/pages/settings.vue
+++ assets/vuejs/pages/settings.vue
@@ -2,8 +2,16 @@
   <div>
     <h1>{{ this.$trans('settings.title', {}, null, true) }}</h1>
 
+    <h2>{{ this.$trans('settings.locale', {}, null, true) }}</h2>
+    <div>
+      <a class="btn btn-dark mr-2" :href="getLocaleUrl(locale)" v-for="locale in locales"
+         :disabled="locale === currentLocale" :class="{'disabled': locale === currentLocale}">
+        <img :src="'/img/flags/'+locale+'.png'" class="mr-2 img-icon-locale" />
+        {{ getLocaleName(locale) }}
+      </a>
+    </div>
 
-    <hr/>
+    <h2>{{ this.$trans('settings.reset', {}, null, true) }}</h2>
     <button class="btn btn-warning" @click="resetState">{{ this.$trans('settings.reset', {}, null, true) }}</button>
 
   </div>
@@ -16,14 +24,32 @@ export default {
   name: "settingsPage",
   data () {
     return {
+      locales: [],
+      currentLocale: ''
     }
   },
+  mounted () {
+    this.locales = Object.keys(URL_LOCALE)
+    this.currentLocale = APP_LOCALE
+  },
   methods: {
-    ...mapActions(['resetState'])
+    ...mapActions(['resetState']),
+    getLocaleName (locale) {
+      if (LOCALE_NAMES[locale] === undefined) return ''
+      return LOCALE_NAMES[locale]
+    },
+    getLocaleUrl (locale) {
+      if (URL_LOCALE[locale] === undefined) return '#'
+      return URL_LOCALE[locale] + '#' + this.$route.fullPath
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+
+.img-icon-locale {
+  height: 30px;
+}
 
 </style>
