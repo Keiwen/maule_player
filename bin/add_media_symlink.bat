83365f6cd47e8adf7c1f307885e8281e3c42c06d
keiwen <keiwen.mk@gmail.com>
2023-10-06 16:45:18 +0200

    fix: MLP-62 Issue on symlink creation with another drive

--- bin/add_media_symlink.bat
+++ bin/add_media_symlink.bat
@@ -3,11 +3,19 @@
 SET medialib=%~dp0..\public\media_lib\
 ECHO This script will create a windows symlink for a targeted media directory, inside media_lib folder
 
+FOR %%i IN ("%medialib%") DO (
+    SET mediadrive=%%~di
+)
+CD \
+%mediadrive%
+CD %medialib%
+
 SET /p mediapath="Enter path to media directory: "
 IF "%mediapath%"=="" EXIT
 IF NOT EXIST "%mediapath%" (
     ECHO Cannot found media path
     PAUSE
+    EXIT
 )
 
 FOR %%i IN ("%mediapath%") DO (
@@ -21,7 +29,6 @@ IF "%linkname%"=="" (
 
 ECHO Media path found, creating symlink...
 
-CD %medialib%
 MKLINK /D "%linkname%" "%mediapath%"
 
 PAUSE
