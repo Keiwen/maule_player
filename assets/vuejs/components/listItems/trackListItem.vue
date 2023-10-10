2f31e86851986966c67746358c8963806c07c378
keiwen <keiwen.mk@gmail.com>
2023-10-10 11:37:39 +0200

    feat: MLP-36 Track page
    add link on track item

--- assets/vuejs/components/listItems/trackListItem.vue
+++ assets/vuejs/components/listItems/trackListItem.vue
@@ -9,25 +9,35 @@
 
     <div class="container-fluid row">
 
-      <div class="col-2 item-icon">
-        <button class="btn btn-primary btn-playlist" @click="selectTrack">
+      <div class="col-2">
+        <router-link :to="{ name: 'track', params: { id: track.id, track: track }}" class="btn btn-primary track-link">
+          <track-icon />
+        </router-link>
+      </div>
+
+      <div class="col-8">
+        <div class="row">
+          <div class="col-12">
+            <span class="track-artist">{{ getLimitedTitle(track.artist.name, 18) }}</span>
+            <span class="track-duration">{{ trackDuration }}</span>
+          </div>
+
+          <div class="col-2">
+            <span class="track-number">#{{ track.trackNumber }}</span>
+          </div>
+          <div class="col-10">
+            <span class="track-album">{{ getLimitedTitle(track.album.name, 13) }}</span>
+            <span class="track-year">{{ track.year }}</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-2">
+        <button class="btn btn-secondary btn-playlist" @click="selectTrack">
           <i class="fa fa-folder-plus" />
         </button>
       </div>
-      <div class="col-10 row">
-        <div class="col-12">
-          <span class="track-artist">{{ getLimitedTitle(track.artist.name, 25) }}</span>
-          <span class="track-duration">{{ trackDuration }}</span>
-        </div>
 
-        <div class="col-2">
-          <span class="track-number">#{{ track.trackNumber }}</span>
-        </div>
-        <div class="col-10">
-          <span class="track-album">{{ getLimitedTitle(track.album.name, 18) }}</span>
-          <span class="track-year">{{ track.year }}</span>
-        </div>
-      </div>
 
     </div>
   </div>
@@ -35,9 +45,11 @@
 
 <script>
 import {mapGetters, mapActions} from "vuex";
+import trackIcon from "../icons/trackIcon";
 
 export default {
   name: "trackListItem",
+  components: { trackIcon },
   props: {
     track: {
       type: Object,
@@ -81,13 +93,20 @@ export default {
 .track-duration {
   position: absolute;
   right: 0;
-  margin-right: -45px;
 }
 
 .track-year {
   position: absolute;
   right: 0;
-  margin-right: -45px;
+}
+
+.track-link {
+  width: 40px;
+  height: 40px;
+  svg {
+    height: 100%;
+    margin-left: -6px;
+  }
 }
 
 
