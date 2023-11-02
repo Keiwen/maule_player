3c2ac5f93036f66d5b65c1205b105ee969297612
keiwen <keiwen.mk@gmail.com>
2023-11-02 12:17:21 +0100

    feat: MLP-104 PWA Audit - Accessibility
    Buttons do not have an accessible name

--- assets/vuejs/components/mediaPlayer/playButton.vue
+++ assets/vuejs/components/mediaPlayer/playButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="divClass" @click="clickOnPlay()">
+  <div :class="divClass" @click="clickOnPlay()" role="button" :aria-label="playing ? 'Pause track' : 'Play track'">
     <svg id="play" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
          viewBox="0 0 100 100" xml:space="preserve">
             <path class="play-border play-border-solid" fill="none" :stroke="color"
